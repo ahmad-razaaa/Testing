dfaa5de6a7f6def08951620d860e2d6227091fac
Ahmad Raza <ahmadrazaar3210@gmail.com>
2025-12-02 06:06:21 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ExpiryInput from './ExpiryInput'
+
+export default function App(){
+  return (
+    <div style={{padding: '2rem', fontFamily: 'sans-serif'}}>
+      <h1>ExpiryInput (Vite)</h1>
+      <ExpiryInput />
+    </div>
+  )
+}
